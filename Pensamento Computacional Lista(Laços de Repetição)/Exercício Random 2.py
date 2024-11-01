b4a8802a1090e016ae8444fbdc141935dad7e23d
Kadu Brum Gavião <gaviaokadu73@gmail.com>
2024-10-31 22:01:50 -0300

    Add files via upload

---
+++ Pensamento Computacional Lista(Laços de Repetição)/Exercício Random 2.py
@@ -0,0 +1,17 @@
+"""
+frutas = ['maça', 'banana', 'cereja', 'abacaxi']
+
+for i in frutas:
+    if i == 'cereja':
+        continue
+    print(i)
+"""
+"""
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+for i in range(num1, num2, 3):
+    print(i)
+"""
+
+
